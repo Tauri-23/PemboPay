f70afaf6abf056c6c96874cdc43af40f3e4ab15e
Tauri-23 <airichjaydiawan@gmail.com>
2024-05-13 00:06:09 +0800

    asd
    asd

--- app/Services/GenerateIdService.php
+++ app/Services/GenerateIdService.php
@@ -5,12 +5,11 @@ use App\Contracts\IGenerateIdService;
 class GenerateIdService implements IGenerateIdService {
     public function generate($modelInstance) {
         do {
-            $randNum = str_pad(mt_rand(1, 999999), 6, '0', STR_PAD_LEFT);
-            $ifExist = $modelInstance::where('id', $randNum)->count();
-        } while($ifExist > 0);
-        
+            $randNum = mt_rand(100000, 999999);
+    
+            $ifExist = $modelInstance::where('id', $randNum)->exists();
+        } while ($ifExist);
+    
         return $randNum;
     }
-
-
 }
