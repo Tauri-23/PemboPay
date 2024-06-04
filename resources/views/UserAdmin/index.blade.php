66629ed302ce4df4502e8a733412d5fe52e3a445
Tauri-23 <airichjaydiawan@gmail.com>
2024-06-04 17:33:35 +0800

    asd
    asd

--- resources/views/UserAdmin/index.blade.php
+++ resources/views/UserAdmin/index.blade.php
@@ -89,7 +89,6 @@
     {{-- Scripts --}}
     <script src="/assets/js/app.js"></script>
     <script src="/assets/js/navbar.js"></script>
-    <script src="/assets/js/treasury-dash.js"></script>
     {{-- bootstrap --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     {{-- chart.js --}}
